6fe32dafdabd73148786c96ab7461eb60c25606d
Peter Beverloo <peter@chromium.org>
2013-01-09 15:28:56 +0000

    Remove cruft from titles I don't care about (currently just on Google Code).

--- markdown.js
+++ markdown.js
@@ -6,6 +6,14 @@ chrome.tabs.getSelected(null, function(tab) {
   // Normalize the title, as auto-link can't handle "Bug X" very well..
   tabTitle = tabTitle.replace(/Bug\s+(\d+)/g, 'Bug  $1');
 
+  // Remove phrases from titles I don't care about, which just add extra cruft.
+  var titlePhrasesToRemove = [
+    ' - An open-source browser project to help move the web forward. - Google Project Hosting'
+  ];
+
+  for (var index = 0, length = titlePhrasesToRemove.length; index < length; ++index)
+    tabTitle = tabTitle.replace(titlePhrasesToRemove[index], '');
+
   // TODO: Remove ampersands from the URL in a way that does not choke the parser.
   markdownCode.value = '[' + tabTitle + '](' + tabUrl + ')';
 });
